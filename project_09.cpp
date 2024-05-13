f3bab704ca6d56ce943ae4453455b2dbe1fbf0fc
RAHUL PRASAD <157337927+Rahwik@users.noreply.github.com>
2024-05-13 11:45:28 +0530

    Update project_09.cpp

--- project_09.cpp
+++ project_09.cpp
@@ -1,3 +1,4 @@
+//MOnster Combat
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
@@ -6,161 +7,194 @@ using namespace std;
 
 int main()
 {
-  int choice;
-  int mhp, hp, i, init, atk, def, matk, mdef, hurt, mhurt, agi, magi;
-  atk = 10;
-  def = 15;
-  agi = 5;
-  matk = 10;
-  mdef = 15;
-  magi = 5;
-  
-  srand((unsigned)time(0));
-  init = rand()%2+1;
-  mhp = rand()%50 + 60;
-  hp = rand()%20 + 80;
-  if (init == 1) {
-  cout<<"You start.\n==========\n";
-  while (hp > 0 || mhp > 0) {
-    cout<<"What do you want to do?\n1 - Fierce Attack\n2 - Lithe Attack\n3 - Defensive moves\n";
-     do{cin>>choice;}while(choice>3 || choice<1);
-    switch (choice) {
-      case 1:
-        atk = rand()%20+10;
-    def = rand()%10+10;
-    agi = rand()%5;
-    break;
-      case 2:
-        atk = rand()%5+10;
-    def = rand()%10+10;
-    agi = rand()%15;
-        break;
-      case 3:
-        atk = rand()%10+10;
-    def = rand()%20+10;
-    agi = rand()%5;
-    break;
-    }
-    choice = rand()%3;
-    switch (choice) {
-      case 1:
-        matk = rand()%20+10;
-    mdef = rand()%10+10;
-    magi = rand()%5;
-    break;
-      case 2:
-        matk = rand()%5+10;
-    mdef = rand()%10+10;
-    magi = rand()%15;
-        break;
-      case 3:
-        matk = rand()%10+10;
-    mdef = rand()%20+10;
-    magi = rand()%5;
-    break;
-    }
+    int playerChoice;
+    int monsterMaxHealth, playerHealth, playerInitiative, playerAttack, playerDefense, playerAgility;
+    int monsterAttack, monsterDefense, monsterMagic;
 
-//Här dör folk o sånt
-    mhurt = (atk - magi) - (mdef/atk);
-    if (mhurt < 0) {
-      mhurt = 0;
-    }
-    mhp = mhp - mhurt;
-    cout<<"You did "<<mhurt<<" damage to the monster!\n";
-    cin.get();
-//Specielt här
-    if (mhp < 1) {
-      cout<<"You killed the beast!! You won with "<<hp<<" hp left.\n";
-      cin.get();
-      return 0;
-      }
-    cout<<"The monster now have "<<mhp<<" hp left.\n";
-    hurt = (matk - agi) - (def/matk);
-    if (hurt < 0) {
-      hurt = 0;
-    }
-    hp = hp - hurt;
-    cout<<"The monster hit you for "<<hurt<<" damage.\n";
-//Och här.
-    if (hp < 1) {
-      cout<<"You died. The beast still has "<<mhp<<" hp left.\n";
-      cin.get();
-      return 0;
-      }
-cout<<"You now have "<<hp<<" hp left.\n\n";
-     }
-     }
+    // Initialize player and monster attributes
+    playerAttack = 10;
+    playerDefense = 15;
+    playerAgility = 5;
+    monsterAttack = 10;
+    monsterDefense = 15;
+    monsterMagic = 5;
 
-//Om monstret startar.
-  else {
-  cout<<"Monster start.\n==============\n";
-    while (hp > 0 || mhp > 0) {
-    choice = rand()%3;
-    switch (choice) {
-      case 1:
-        matk = rand()%20+10;
-    mdef = rand()%10+10;
-    magi = rand()%5;
-    break;
-      case 2:
-        matk = rand()%5+10;
-    mdef = rand()%10+10;
-    magi = rand()%15;
-        break;
-      case 3:
-        matk = rand()%10+10;
-    mdef = rand()%20+10;
-    magi = rand()%5;
-    break;
-    }
-//Monstret börjar!! han slår till direkt.
-    hurt = (matk - agi) - (def/matk);
-    if (hurt < 0) {
-      hurt = 0;
-    }
-    hp = hp - hurt;
-    cout<<"The monster hit you for "<<hurt<<" damage.\n";
-//Oooooh, gotta hurt!
-    if (hp < 1) {
-      cout<<"You died. The beast still has "<<mhp<<" hp left.\n";
-      cin.get();
-      return 0;
-      }
- cout<<"You now have "<<hp<<" hp left.\n\n";
-    cout<<"What do you want to do?\n1 - Fierce Attack\n2 - Lithe Attack\n3 - Defensive moves\n";
-     do{cin>>choice;}while(choice>3 || choice<1);
-    switch (choice) {
-      case 1:
-        atk = rand()%20+10;
-    def = rand()%10+10;
-    agi = rand()%5;
-    break;
-      case 2:
-        atk = rand()%5+10;
-    def = rand()%10+10;
-    agi = rand()%15;
-        break;
-      case 3:
-        atk = rand()%10+10;
-    def = rand()%20+10;
-    agi = rand()%5;
-    break;
+    // Seed the random number generator
+    srand((unsigned)time(0));
+
+    // Randomly determine who starts the battle
+    playerInitiative = rand() % 2 + 1;
+
+    // Randomly assign health points to the player and the monster
+    monsterMaxHealth = rand() % 50 + 60;
+    playerHealth = rand() % 20 + 80;
+
+    // Player starts the battle
+    if (playerInitiative == 1) {
+        cout << "You start.\n==========\n";
+
+        // Battle loop
+        while (playerHealth > 0 || monsterMaxHealth > 0) {
+            // Player's turn
+            cout << "What do you want to do?\n1 - Fierce Attack\n2 - Lithe Attack\n3 - Defensive moves\n";
+            do { cin >> playerChoice; } while (playerChoice > 3 || playerChoice < 1);
+
+            // Randomly determine player's attack attributes based on choice
+            switch (playerChoice) {
+            case 1:
+                playerAttack = rand() % 20 + 10;
+                playerDefense = rand() % 10 + 10;
+                playerAgility = rand() % 5;
+                break;
+            case 2:
+                playerAttack = rand() % 5 + 10;
+                playerDefense = rand() % 10 + 10;
+                playerAgility = rand() % 15;
+                break;
+            case 3:
+                playerAttack = rand() % 10 + 10;
+                playerDefense = rand() % 20 + 10;
+                playerAgility = rand() % 5;
+                break;
+            }
+
+            // Randomly determine monster's attack attributes
+            playerChoice = rand() % 3;
+            switch (playerChoice) {
+            case 1:
+                monsterAttack = rand() % 20 + 10;
+                monsterDefense = rand() % 10 + 10;
+                monsterMagic = rand() % 5;
+                break;
+            case 2:
+                monsterAttack = rand() % 5 + 10;
+                monsterDefense = rand() % 10 + 10;
+                monsterMagic = rand() % 15;
+                break;
+            case 3:
+                monsterAttack = rand() % 10 + 10;
+                monsterDefense = rand() % 20 + 10;
+                monsterMagic = rand() % 5;
+                break;
+            }
+
+            // Player attacks monster
+            int monsterHurt = (playerAttack - monsterMagic) - (monsterDefense / playerAttack);
+            if (monsterHurt < 0) {
+                monsterHurt = 0;
+            }
+            monsterMaxHealth = monsterMaxHealth - monsterHurt;
+            cout << "You did " << monsterHurt << " damage to the monster!\n";
+
+            // Check if monster is defeated
+            if (monsterMaxHealth < 1) {
+                cout << "You killed the beast!! You won with " << playerHealth << " hp left.\n";
+                cin.get();
+                return 0;
+            }
+
+            cout << "The monster now has " << monsterMaxHealth << " hp left.\n";
+
+            // Monster attacks player
+            int playerHurt = (monsterAttack - playerAgility) - (playerDefense / monsterAttack);
+            if (playerHurt < 0) {
+                playerHurt = 0;
+            }
+            playerHealth = playerHealth - playerHurt;
+            cout << "The monster hit you for " << playerHurt << " damage.\n";
+
+            // Check if player is defeated
+            if (playerHealth < 1) {
+                cout << "You died. The beast still has " << monsterMaxHealth << " hp left.\n";
+                cin.get();
+                return 0;
+            }
+
+            cout << "You now have " << playerHealth << " hp left.\n\n";
         }
-
-
-//Här kan han dö.
-    mhurt = (atk - magi) - (mdef/atk);
-    if (mhurt < 0) {
-      mhurt = 0;
     }
-    mhp = mhp - mhurt;
-    cout<<"You did "<<mhurt<<" damage to the monster!\n";
-    cin.get();
-//Eller typ här:
-    if (mhp < 1) {
-      cout<<"You killed the beast!! You won with "<<hp<<" hp left.\n";
-      cin.get();
-      return 0;
-      }
-    cout<<"The monster now have "<<mhp<<" hp left.\n";
-  } } }
+    // Monster starts the battle
+    else {
+        cout << "Monster starts.\n==============\n";
+
+        // Battle loop
+        while (playerHealth > 0 || monsterMaxHealth > 0) {
+            // Randomly determine monster's attack attributes
+            playerChoice = rand() % 3;
+            switch (playerChoice) {
+            case 1:
+                monsterAttack = rand() % 20 + 10;
+                monsterDefense = rand() % 10 + 10;
+                monsterMagic = rand() % 5;
+                break;
+            case 2:
+                monsterAttack = rand() % 5 + 10;
+                monsterDefense = rand() % 10 + 10;
+                monsterMagic = rand() % 15;
+                break;
+            case 3:
+                monsterAttack = rand() % 10 + 10;
+                monsterDefense = rand() % 20 + 10;
+                monsterMagic = rand() % 5;
+                break;
+            }
+
+            // Monster attacks player
+            int playerHurt = (monsterAttack - playerAgility) - (playerDefense / monsterAttack);
+            if (playerHurt < 0) {
+                playerHurt = 0;
+            }
+            playerHealth = playerHealth - playerHurt;
+            cout << "The monster hit you for " << playerHurt << " damage.\n";
+
+            // Check if player is defeated
+            if (playerHealth < 1) {
+                cout << "You died. The beast still has " << monsterMaxHealth << " hp left.\n";
+                cin.get();
+                return 0;
+            }
+
+            cout << "You now have " << playerHealth << " hp left.\n\n";
+
+            // Player's turn
+            cout << "What do you want to do?\n1 - Fierce Attack\n2 - Lithe Attack\n3 - Defensive moves\n";
+            do { cin >> playerChoice; } while (playerChoice > 3 || playerChoice < 1);
+
+            // Randomly determine player's attack attributes based on choice
+            switch (playerChoice) {
+            case 1:
+                playerAttack = rand() % 20 + 10;
+                playerDefense = rand() % 10 + 10;
+                playerAgility = rand() % 5;
+                break;
+            case 2:
+                playerAttack = rand() % 5 + 10;
+                playerDefense = rand() % 10 + 10;
+                playerAgility = rand() % 15;
+                break;
+            case 3:
+                playerAttack = rand() % 10 + 10;
+                playerDefense = rand() % 20 + 10;
+                playerAgility = rand() % 5;
+                break;
+            }
+
+            // Player attacks monster
+            int monsterHurt = (playerAttack - monsterMagic) - (monsterDefense / playerAttack);
+            if (monsterHurt < 0) {
+                monsterHurt = 0;
+            }
+            monsterMaxHealth = monsterMaxHealth - monsterHurt;
+            cout << "You did " << monsterHurt << " damage to the monster!\n";
+
+            // Check if monster is defeated
+            if (monsterMaxHealth < 1) {
+                cout << "You killed the beast!! You won with " << playerHealth << " hp left.\n";
+                cin.get();
+                return 0;
+            }
+
+            cout << "The monster now has " << monsterMaxHealth << " hp left.\n";
+        }
+    }
+}
