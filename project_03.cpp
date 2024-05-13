a50c39fffa93fdb385afcb00d645054a1411d38c
RAHUL PRASAD <157337927+Rahwik@users.noreply.github.com>
2024-05-13 11:40:27 +0530

    Update project_03.cpp

--- project_03.cpp
+++ project_03.cpp
@@ -1,3 +1,4 @@
+//Guess the Number
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
@@ -5,24 +6,24 @@ using namespace std;
 
 int main()
 {
-    int num, guess, tries = 0;
-    srand(time(0));
-    num = rand() % 100 + 1;
-    cout << "Guess My Number Game\n\n";
+    int num, guess, tries = 0; // Declare variables to store the random number, user's guess, and number of tries
+    srand(time(0)); // Seed the random number generator with the current time
+    num = rand() % 100 + 1; // Generate a random number between 1 and 100
+    cout << "Guess My Number Game\n\n"; // Print game title
 
     do
     {
-        cout << "Enter a guess between 1 and 100 : ";
-        cin >> guess;
-        tries++;
+        cout << "Enter a guess between 1 and 100 : "; // Prompt the user to enter a guess
+        cin >> guess; // Read user's input
+        tries++; // Increment the number of tries
 
         if (guess > num)
-            cout << "Too high!\n\n";
+            cout << "Too high!\n\n"; // If guess is too high, print a message
         else if (guess < num)
-            cout << "Too low!\n\n";
+            cout << "Too low!\n\n"; // If guess is too low, print a message
         else
-            cout << "\nCorrect! You got it in " << tries << " guesses!\n";
-    } while (guess != num);
+            cout << "\nCorrect! You got it in " << tries << " guesses!\n"; // If guess is correct, print a message with the number of tries
+    } while (guess != num); // Continue loop until the guess matches the random number
 
     return 0;
 }
